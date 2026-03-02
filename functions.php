64e579c4778ed30360e50372bb37136a95c2dfc2
Murata MacBookAir <murata@interplan-inc.com>
2026-03-02 16:49:51 +0900

    0302_70p

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+// functions.phpの「phpのコード」は**絶対に閉じない**
+
+// <title>タグの出力
+add_theme_support('title-tag');
+
+// アイキャッチの有効化
+add_theme_support('post-thumbnails');
+
+// <title>タグの区切り文字の変更
+add_filter('document_title_separator','my_document_title_separator');
+function my_document_title_separator($separator){
+  // ''で囲われている文字列が区切り文字になるよ
+  $separator = '|';
+  return $separator;
+}
